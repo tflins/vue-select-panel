32a3299107b768395599242ff5d7ac96bc6ec48f
tflin <tflins@163.com>
2020-03-24 17:52:21 +0800

    基础架构

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <vue-select-panle></vue-select-panle>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import VueSelectPanle from '@/components/VueSelectPanle'
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    VueSelectPanle
   }
 }
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style rel="stylesheet/scss" lang="scss" scoped>
+
 </style>
---
+++ src/components/VueSelectPanle/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="vue-select-panle">
+    <div class="vue-select-panle-header">
+      <div class="vue-select-panle-header__title">
+        文章分类
+      </div>
+      <div class="vue-select-panle-header__question">
+        <img src="@/assets/icon/question.png">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VueSelectPanle',
+
+  data() {
+
+  },
+
+  props: {
+    dataList: {
+      
+    }
+  }
+}
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+
+</style>
