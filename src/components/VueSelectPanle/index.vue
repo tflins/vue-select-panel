a15a8c4ab904710925910dec98bfcdb1ae94341e
tflin <tflins@163.com>
2020-03-24 18:23:12 +0800

    完成 header 样式

--- src/components/VueSelectPanle/index.vue
+++ src/components/VueSelectPanle/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="vue-select-panle">
+
     <div class="vue-select-panle-header">
       <div class="vue-select-panle-header__title">
         文章分类
@@ -7,7 +8,15 @@
       <div class="vue-select-panle-header__question">
         <img src="@/assets/icon/question.png">
       </div>
+      <div class="vue-select-panle-header__selectcount">
+        5/5
+      </div>
     </div>
+
+    <div class="vue-select-panle-body">
+
+    </div>
+
   </div>
 </template>
 
@@ -15,18 +24,64 @@
 export default {
   name: 'VueSelectPanle',
 
-  data() {
-
-  },
+  data() {},
 
   props: {
-    dataList: {
-      
-    }
+    dataList: {}
   }
 }
 </script>
 
 <style rel="stylesheet/scss" lang="scss" scoped>
+.vue-select-panle {
+  box-sizing: border-box;
+  position: relative;
+  width: 278px;
+  height: 181px;
+  padding: 20px 16px;
 
+  background-color: rgba(255, 255, 255, 1);
+  border: 1px solid rgba(230, 232, 235, 0.98);
+  border-radius: 4px;
+  font-family: PingFangSC-Medium, PingFang SC;
+
+  &-header {
+    display: flex;
+    align-items: center;
+
+    &__title,
+    &__question {
+      float: left;
+    }
+
+    &__title {
+      margin-right: 5px;
+      font-size: 18px;
+      font-weight: 500;
+      color: rgba(24, 25, 26, 1);
+      line-height: 25px;
+    }
+
+    &__question {
+      width: 18px;
+      height: 18px;
+      overflow: hidden;
+
+      img {
+        width: 100%;
+        height: 100%;
+      }
+    }
+
+    &__selectcount {
+      margin-left: auto;
+
+      color: rgba(121, 124, 128, 1);
+      font-size: 14px;
+      font-weight: 400;
+      line-height: 20px;
+      font-family: PingFangSC-Regular, PingFang SC;
+    }
+  }
+}
 </style>
